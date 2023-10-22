00849b246df8315917776643b1ad7a4276995a66
ariceliom <contato@ariceliomoura.com>
2023-10-21 22:33:26 -0300

    up

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+
+  <meta name="HandheldFriendly" content="true">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+
+  <!-- Título do site -->
+  <title>JC Barber Shop | Conseito e Estilo</title>
+  <meta property="og:title" content="JC Barber Shop | Conseito e Estilo">
+
+  <!-- Cor tema do site -->
+  <meta name="theme-color" content="#06090c">
+  <meta name="msapplication-TileColor" content="#06090c">
+  <meta name="msapplication-navbutton-color" content="#06090c">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#06090c">
+
+  <!-- Favicons do site -->
+  <link rel="icon" type="image/png" sizes="16x16" href="./files/favicons/favicon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./files/favicons/favicon.png">
+  <link rel="icon" type="image/png" sizes="194x194" href="./files/favicons/favicon.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="./files/favicons/favicon.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="./files/favicons/favicon.png">
+  <link rel="mask-icon" href="./files/favicons/safari-pinned-tab.svg" color="#06090c">
+  <link rel="shortcut icon" href="./files/favicons/favicon.png">
+  <meta name="msapplication-TileImage" content="./files/favicons/favicon.png">
+
+  <!-- Tamanho imagem compartilhamento -->
+  <meta property="og:locale" content="pt_BR">
+  <meta property="og:image:type" content="image/png">
+  <meta property="og:image:width" content="1920">
+  <meta property="og:image:height" content="1080">
+  <meta property="og:type" content="website">
+  <meta property="og:card" content="summary_large_image">
+
+  <!-- Imagem de compartilhamento -->
+  <meta name="image" content="./files/favicons/favicon.png">
+  <meta itemprop="image" content="./files/favicons/favicon.png">
+  <meta property="og:image" content="./files/favicons/favicon.png">
+
+  <!-- Nome do Site -->
+  <meta name="author" content="JC Barber Shop | Conseito e Estilo">
+  <meta itemprop="name" content="JC Barber Shop | Conseito e Estilo">
+  <meta name="article:author" content="JC Barber Shop | Conseito e Estilo">
+  <meta name="application-name" content="JC Barber Shop | Conseito e Estilo">
+  <meta property="og:site_name" content="JC Barber Shop | Conseito e Estilo">
+  <meta name="apple-mobile-web-app-title" content="JC Barber Shop | Conseito e Estilo">
+
+  <!-- Importação do CSS -->
+  <link href="./assets/css/pages/index/index.css" rel="stylesheet">
+</head>
+<body>
+
+  <!-- Seção -->
+  <section class="apresentacao">
+    <!-- Efeito de Faixa -->
+    <div class="faixa"></div>
+    <!-- Conteúdo -->
+    <div class="conteudo">
+      <!-- Efeito de Blush -->
+      <div class="blush"></div>
+      <!-- Foto -->
+      <div class="foto">
+        <div class="linha"></div>
+        <img src="./files/images/perfil.jpg">
+      </div>
+      <!-- Informação -->
+      <div class="informacao">
+        <h2>José Carlos | Barber Shop</h2>
+        <p>Conseito em estilo</p>
+      </div>
+      <!-- Links -->
+      <div class="links">
+        <!-- Link -->
+        <a href="https://www.instagram.com/josecarlosbarbershopp/" target="_blank">
+          <div class="icone">
+            <img src="./files/images/icone-instagram.png">
+          </div>
+          <div class="texto">
+            <h3>@josecarlosbarbershopp</h3>
+            <p>Siga meu perfil</p>
+          </div>
+        </a>
+        <!-- Link -->
+        <a href="https://jcbarbershop.ariceliomoura.com/" target="_blank">
+          <div class="icone">
+            <img src="./files/images/icone-site.png">
+          </div>
+          <div class="texto">
+            <h3>jcbarbershop.com</h3>
+            <p>Acesse meu site</p>
+          </div>
+        </a>
+        <!-- Link -->
+        <a href="https://api.whatsapp.com/send/?phone=5585986279194&text=Ol%C3%A1%2C+gostaria+de+agendar+um+hor%C3%A1rio%21&type=phone_number&app_absent=0" target="_blank">
+          <div class="icone">
+            <img src="./files/images/icone-whatsapp.png">
+          </div>
+          <div class="texto">
+            <h3>Agende um horário</h3>
+            <p>Chame no Whatsapp</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <!-- Importação do Javascript -->
+  <script src="./assets/js/global/global.js"></script>
+
+</body>
+</html>
